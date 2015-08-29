60207abba623fe783306a01d59d93b7d29801541
darpe <darwin5751@gmail.com>
2015-08-29 17:31:50 +0200

    mod 2 29/8/2015

--- js/script.js
+++ js/script.js
@@ -1,16 +1,32 @@
 function ejecuta(){
-	
-	for (var i=0; i < 5; i++) {
+
+	var elementos=document.querySelectorAll("#principal p, span");
+
+		for(var i = 0; i < elementos.length; i++) {
+
+			elementos[i].onclick=saludo;
+
+	/*for (var i = 0; i < 5; i++) {
+
+		var elementos=document.querySelectorAll("#principal p");
+		elementos[i].onclick=saludo;
+
+	/*var elementos = document.querySelectorAll("#principal p")[2].onclick=saludo; /*seleccionar un solo elemento*/
+
+	/*document.querySelector("#principal p:last-child").onclick=saludo;
+
+	/*for (var  i = 0; i < 5; i++) {
 	var a = document.GetElementsbyClassName("dos")[i].onclick=saludo;
 
 	/*for (var i = 0; i < 5; i++) {
 	document.getElementsByTagName('p')[i].onclick=saludo; /*para un solo elemento*/
 	/*document.getElementById("dos").onclick=saludo; /*para id*/
 	}
+
 }
 
 function saludo(){
 	alert("que hay de nuevo");
-}	
+}
 
 window.onload=ejecuta;
