024a3195d34dcb4a2579b9ea849c041e396d4d7a
darpe <darwin5751@gmail.com>
2015-08-31 22:00:22 +0200

    mod 1 31/08/2015

---
+++ js/script_funcionabilidad_audio.js
@@ -0,0 +1,42 @@
+var miaudio, reproducir, barra, progreso, maximo;
+maximo = 600;
+
+function comenzar() {
+	miaudio = document.getElementById("miaudio");
+	reproducir = document.getElementById("play");
+	barra = document.getElementById("barra");
+	progreso = document.getElementById("progreso");
+		reproducir.addEventListener("click",clicando, false);
+		barra.addEventListener("click",adelantando, false);
+}
+
+function clicando() {
+	if ((miaudio.paused==false) && (miaudio.ended==false)){
+		miaudio.pause();
+		reproducir.innerHTML="Play";
+	}
+		else{
+			miaudio.play();
+			reproducir.innerHTML="pause"; /*cambiarle texto*/
+			bucle = setInterval(estado, 50); /*el numero es igual a los miles segundo que hace el llamado*/
+		}
+}
+
+function estado(){
+	if (miaudio.ended==false){
+		var total = parseInt(miaudio.currentTime*maximo/miaudio.duration);
+		progreso.style.width = total+"px";
+	}
+}
+
+function adelantando(posicion){
+
+	if((miaudio.paused==false) && (miaudio.ended==false)){
+		var ratonX=posicion.pageX-barra.offsetLeft;
+		var nuevoTiempo=ratonX*miaudio.duration/maximo;
+			miaudio.currentTime=nuevoTiempo;
+			progreso.style.width=ratonX+"px";
+	}
+}
+
+window.addEventListener("load", comenzar, false);
